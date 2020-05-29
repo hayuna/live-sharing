0537599c9a0284d34784b57412113f245a4cde8c
Hayuna <marek.kowalonek@gmail.com>
2020-05-29 16:08:28 +0200

    rename variable to more semantic

--- src/redux/user.reducer.js
+++ src/redux/user.reducer.js
@@ -1,39 +1,46 @@
 import { actionTypes } from "./action.types";
 
-const user = {
-    currentUser: {},
-    users: []
-}
+const initialState = {
+  currentUser: {},
+  users: [],
+};
 
-const userReducer = (state = user, action) => {
-    switch (action.type) {
-        case actionTypes.ADD_USER: return {
-            ...state,
-            login: action.login
-        }
-        case actionTypes.ADD_EMAIL: return {
-            ...state,
-            email: action.email
-        };
-        case actionTypes.ADD_DATE_OF_BIRTH: return {
-            ...state,
-            dateOfBirth: action.dateOfBirth
-        };
-        case actionTypes.SET_CURRENT_USER: return {
-            ...state,
-            currentUser: action.user
-        };
-        case actionTypes.ADD_NEW_USER: return {
-            ...state,
-            users: [...state.users, action.user]
-        };
-        case actionTypes.DELETE_USER: return {
-            ...state,
-            user: action.user
-        };
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_USER:
+      return {
+        ...state,
+        login: action.login,
+      };
+    case actionTypes.ADD_EMAIL:
+      return {
+        ...state,
+        email: action.email,
+      };
+    case actionTypes.ADD_DATE_OF_BIRTH:
+      return {
+        ...state,
+        dateOfBirth: action.dateOfBirth,
+      };
+    case actionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.user,
+      };
+    case actionTypes.ADD_NEW_USER:
+      return {
+        ...state,
+        users: [...state.users, action.user],
+      };
+    case actionTypes.DELETE_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
 
-        default: return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
-export default userReducer
+export default userReducer;
